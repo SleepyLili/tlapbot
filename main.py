1d3bce203c43ce5dc84c711180544ff4db7a8479
Lili Pavelu <pastrnakie@seznam.cz>
2022-05-25 13:24:55 +0200

    listening to local owncast now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,json
+import requests
 
 app = Flask(__name__)
 
@@ -12,6 +13,15 @@ def githubIssue():
     print(f'Issue {data["issue"]["title"]} {data["action"]} \n')
     print(f'{data["issue"]["body"]} \n')
     print(f'{data["issue"]["url"]} \n')
+    return data
+
+@app.route('/owncastWebhook',methods=['POST'])
+def owncastWebhook():
+    data = request.json
+    if data["type"] == "CHAT":
+        print("New chat message:")
+        print(f'from {data["eventData"]["user"]["displayName"]}:')
+        print(f'{data["eventData"]["body"]}')
     return data
 
 if __name__ == '__main__':
