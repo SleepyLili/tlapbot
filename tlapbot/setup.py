a922e52f3304c4f95b9b8ebcb5f516c090113a8a
Lili <pastrnakie@seznam.cz>
2022-09-22 13:44:47 +0200

    make project installable

---
+++ tlapbot/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tlapbot',
+    version='0.5.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'requests',
+        'apscheduler'
+    ],
+)
