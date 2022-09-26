951dc38496b347aee7fc71d510a40bf5da0f0e9f
Lili <pastrnakie@seznam.cz>
2022-09-26 16:51:44 +0200

    redeem dashboard now shows in local timezone

--- tlapbot/owncast_redeem_dashboard.py
+++ tlapbot/owncast_redeem_dashboard.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint
 from tlapbot.db import get_db
 from tlapbot.owncast_helpers import pretty_redeem_queue
+from datetime import datetime, timezone
 
 bp = Blueprint('redeem_dashboard', __name__)
 
@@ -8,8 +9,12 @@ bp = Blueprint('redeem_dashboard', __name__)
 def dashboard():
     queue = pretty_redeem_queue(get_db())
     number_of_drinks = 0
+    utc_timezone = timezone.utc
     if queue is not None:
         for row in queue:
             if row[1] == "drink":
                 number_of_drinks += 1
-    return render_template('dashboard.html', queue=queue, number_of_drinks=number_of_drinks)
+    return render_template('dashboard.html',
+                           queue=queue,
+                           number_of_drinks=number_of_drinks,
+                           utc_timezone=utc_timezone)
