c8cc529045f93250546323d0bcf4ce6f87dd6bb0
Lili <pastrnakie@seznam.cz>
2022-10-03 12:32:43 +0200

    bump version after bugfix for tuesday stream

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='0.5.2',
+    version='0.5.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
