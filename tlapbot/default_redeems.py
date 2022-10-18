f0077c56dc853ca3b25ab9cff4b4540c2a605ab2
Lili <pastrnakie@seznam.cz>
2022-10-18 15:39:04 +0200

    fix default redeems

--- tlapbot/default_redeems.py
+++ tlapbot/default_redeems.py
@@ -1,6 +1,6 @@
 REDEEMS={
     "hydrate": {"price": 60, "type": "list"},
-    "lurk": {"price:": 1, "type": "counter"},
-    "react": {"price:": 200, "type": "note"},
-    "request": {"price:": 100, "type": "note"}
+    "lurk": {"price": 1, "type": "counter"},
+    "react": {"price": 200, "type": "note"},
+    "request": {"price": 100, "type": "note"}
 }
