62ddd198349c455827e71aa7cac2c0f8b34b1706
Lili <pastrnakie@seznam.cz>
2022-11-03 16:14:31 +0100

    bump version number for live instance

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='0.6.0a4',
+    version='0.6.0a5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
