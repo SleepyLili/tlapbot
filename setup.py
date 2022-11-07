60e61728b817789a984f1deabf4ab07b21789097
Lili <pastrnakie@seznam.cz>
2022-11-07 12:57:43 +0100

    make version number not be an alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='0.6.0a5',
+    version='0.6.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
