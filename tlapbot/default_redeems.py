f0b918aa2cb44d9a82356476df0e7a3aefbb0583
Lili <pastrnakie@seznam.cz>
2022-11-09 14:35:45 +0100

    add tabs to dashboard tab with recent redeems and counters then tab with redeems info recent redeems open by default

--- tlapbot/default_redeems.py
+++ tlapbot/default_redeems.py
@@ -1,6 +1,6 @@
 REDEEMS={
     "hydrate": {"price": 60, "type": "list"},
-    "lurk": {"price": 1, "type": "counter"},
+    "lurk": {"price": 1, "type": "counter", "info": "Let everyone know you're going to lurk."},
     "react": {"price": 200, "type": "note", "info": "Attach a link to a video for me to react to."},
     "request": {"price": 100, "type": "note", "info": "Request a gamemode, level, skin, etc."}
 }
