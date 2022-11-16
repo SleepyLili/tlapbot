6b34274bc59731f5cf442fbe97f54fa52a2a27e4
Frostyfrog <frostyfrog2@gmail.com>
2022-11-16 05:57:56 -0700

    fix: Load config and redeems from instance folder
    According to the README, both the redeems and the configuration live
    within the same folder as the default database location. However, the
    code did not load them from the instance folder. This commit aligns the
    code to the documentation.
    
    Signed-off-by: Frostyfrog <frostyfrog2@gmail.com>

--- tlapbot/__init__.py
+++ tlapbot/__init__.py
@@ -5,6 +5,11 @@ from tlapbot.db import get_db
 from tlapbot.owncast_helpers import is_stream_live, give_points_to_chat
 
 
+def get_instance(app, path):
+    """Return the path with the instance folder as it's parent."""
+
+    return os.path.join(app.instance_path, path)
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -17,12 +22,12 @@ def create_app(test_config=None):
     # Prepare config: set db to instance folder, then load default, then
     # overwrite it with config.py and redeems.py
     app.config.from_mapping(
-        DATABASE=os.path.join(app.instance_path, "tlapbot.sqlite")
+        DATABASE=get_instance(app, "tlapbot.sqlite")
     )
     app.config.from_object('tlapbot.default_config')
     app.config.from_object('tlapbot.default_redeems')
-    app.config.from_pyfile('config.py', silent=True)
-    app.config.from_pyfile('redeems.py', silent=True)
+    app.config.from_pyfile(get_instance(app, 'config.py'), silent=True)
+    app.config.from_pyfile(get_instance(app, 'redeems.py'), silent=True)
 
     # prepare webhooks and redeem dashboard blueprints
     from . import owncast_webhooks
