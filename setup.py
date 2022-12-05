c6d94f9d5fd7636b1023f0a5cf86aad9651350b6
Lili <pastrnakie@seznam.cz>
2022-12-05 16:06:39 +0100

    version 1.0.1alpha for my next stream

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='1.0.0',
+    version='1.0.1a1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
