05f28b71bdb23862727b36fa10df9a37cec6ef92
Lili <pastrnakie@seznam.cz>
2023-01-10 12:23:59 +0100

    promote develop build to 1.1.0a1 very early alpha, still missing 2 things from the checklist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='1.0.1',
+    version='1.1.0a1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
