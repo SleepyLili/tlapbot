ae792192e87ecabdeec8846ad2addb5758c9d91c
Lili <pastrnakie@seznam.cz>
2023-01-13 11:04:30 +0100

    add a category example to default redeems

--- tlapbot/default_redeems.py
+++ tlapbot/default_redeems.py
@@ -3,4 +3,5 @@ REDEEMS={
     "lurk": {"price": 1, "type": "counter", "info": "Let us know you're going to lurk."},
     "react": {"price": 200, "type": "note", "info": "Attach link to a video for me to react to."},
     "request": {"price": 100, "type": "note", "info": "Request a level, gamemode, skin, etc."}
+    "inactive": {"price": 100, "type": "note", "info": "Example redeem that is inactive by default", "category": "inactive"}
 }
