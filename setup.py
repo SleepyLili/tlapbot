a5b38865ebd7b31111bf79f4e71b6273635c978e
Lili <pastrnakie@seznam.cz>
2023-01-18 16:44:36 +0100

    promote beta build to full release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='1.1.0b1',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
