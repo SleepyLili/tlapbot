265a6cc9b432f8244c6f1623630e76889cddbc6a
Lili <pastrnakie@seznam.cz>
2023-03-27 16:30:24 +0200

    fix check for more than have, add error chat

--- tlapbot/redeems_handler.py
+++ tlapbot/redeems_handler.py
@@ -56,11 +56,13 @@ def handle_redeem(message, user_id):
             send_chat(f"Cannot redeem {redeem}, amount of points is not an integer.")
         elif int(note) < price:
             send_chat(f"Can't redeem {redeem}, your donation is below the minimum bid of {price}.")
-        elif int(note) < points:
+        elif int(note) > points:
             send_chat(f"Can't redeem {redeem}, you're donating more points than you have.")
         elif add_to_milestone(db, user_id, redeem, int(note)):
             send_chat(f"Succesfully donated to {redeem} milestone!")
             if check_apply_milestone_completion(db, redeem):
                 send_chat(f"Milestone goal {redeem} complete!")
+        else:
+            send_chat(f"Redeeming {redeem} failed.")
     else:
         send_chat(f"{redeem} not redeemed, type of redeem not found.")
