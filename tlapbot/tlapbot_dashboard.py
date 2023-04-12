782097b69938b07da3507e6eb9379e2f48f940a1
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-12 13:05:36 +0200

    Add import `redirect`.

--- tlapbot/tlapbot_dashboard.py
+++ tlapbot/tlapbot_dashboard.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request, current_app
+from flask import render_template, Blueprint, request, current_app, redirect
 from tlapbot.db import get_db
 from tlapbot.redeems import all_active_counters, all_active_milestones, all_active_redeems, pretty_redeem_queue
 from tlapbot.owncast_helpers import  read_all_users_with_username
