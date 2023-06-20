6f6fc10d0fddb88b9d3e9e46a306d8906999582a
Lili <pastrnakie@seznam.cz>
2023-06-20 14:39:19 +0200

    remove minimum bid for #15

--- tlapbot/default_redeems.py
+++ tlapbot/default_redeems.py
@@ -3,6 +3,6 @@ REDEEMS={
     "lurk": {"price": 1, "type": "counter", "info": "Let us know you're going to lurk."},
     "react": {"price": 200, "type": "note", "info": "Attach link to a video for me to react to."},
     "request": {"price": 100, "type": "note", "info": "Request a level, gamemode, skin, etc."},
-    "go_nap": {"price": 1, "type": "milestone", "info": "Streamer will go nap when the goal is reached.", "goal": 1000},
+    "go_nap": {"type": "milestone", "info": "Streamer will go nap when the goal is reached.", "goal": 1000},
     "inactive": {"price": 100, "type": "note", "info": "Example redeem that is inactive by default", "category": ["inactive"]}
 }
--- tlapbot/redeems_handler.py
+++ tlapbot/redeems_handler.py
@@ -57,8 +57,6 @@ def handle_redeem(message, user_id):
             send_chat(f"Cannot redeem {redeem}, no amount of points specified.")
         elif not note.isdigit():
             send_chat(f"Cannot redeem {redeem}, amount of points is not an integer.")
-        elif int(note) < price:
-            send_chat(f"Can't redeem {redeem}, your donation is below the minimum bid of {price}.")
         elif int(note) > points:
             send_chat(f"Can't redeem {redeem}, you're donating more points than you have.")
         elif add_to_milestone(db, user_id, redeem, int(note)):
