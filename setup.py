1abab80da446edd88645cc8282ff57704e15cf8a
Lili <pastrnakie@seznam.cz>
2024-02-06 18:21:38 +0100

    promote to 1.2.2a1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='1.2.1',
+    version='1.2.2a1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
