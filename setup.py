2284fb3a00c204d5e53c42c1c5cb99f5a0cdb5a8
Lili <pastrnakie@seznam.cz>
2024-02-11 20:17:17 +0100

    1.2.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tlapbot',
-    version='1.2.2a2',
+    version='1.2.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
